c533dad3e4b7741f783ac0c829b5142d0b549f2b
Noah Hensley <noahehensley@gmail.com>
2024-01-21 16:54:53 -0500

    Reupdated main.py with wifi functionality. First commit was overwritten.

--- src/BMP280/main.py
+++ src/BMP280/main.py
@@ -11,7 +11,7 @@ import adafruit_bmp280
 
 TIME_DELAY = 2 # seconds
 RELATIVE_PRESSURE_AT_SEA_LEVEL = 1040 # hPa
-WIFI_ALTITUDE = 1524 # meters
+CUTOFF_ALTITUDE = 1524 # meters
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -24,7 +24,10 @@ bmp280.sea_level_pressure = RELATIVE_PRESSURE_AT_SEA_LEVEL # Ironton, OH
 while True:
     print("%0.1f,%0.4f,%i" % (bmp280.temperature, bmp280.pressure / 1000, bmp280.altitude))
     
-    # Put wifi logic here
+    if bmp280.altitude >= CUTOFF_ALTITUDE
+        disable_wifi()
+    else:
+        enable_wifi()
     
     time.sleep(TIME_DELAY) # two seconds
 
