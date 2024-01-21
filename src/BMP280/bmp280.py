a8714ee345c7f06e06d653582759e0bf8a169a7b
Noah Hensley <noahehensley@gmail.com>
2024-01-21 16:35:35 -0500

    Added wifi functionality to BMP280.

--- src/BMP280/bmp280.py
+++ src/BMP280/bmp280.py
@@ -24,7 +24,10 @@ bmp280.sea_level_pressure = RELATIVE_PRESSURE_AT_SEA_LEVEL # Ironton, OH
 while True:
     print("%0.1f,%0.4f,%i" % (bmp280.temperature, bmp280.pressure / 1000, bmp280.altitude))
     
-    # Put wifi logic here
+    if bmp280.altitude >= WIFI_ALTITUDE:
+        disable_wifi()
+    else
+        enable_wifi()
     
     time.sleep(TIME_DELAY) # two seconds
 
--- src/BMP280/wifi.py
+++ src/BMP280/wifi.py
@@ -1,10 +1,20 @@
 import subprocess
 import time
 
+def is_wifi_enabled():
+    result = subprocess.run(['sudo', 'ifconfig', 'wlan0'], capture_output=True, text=True)
+    return 'UP' in result.stdout
+
 def enable_wifi():
-    subprocess.run(['sudo', 'ifconfig', 'wlan0', 'up'])
-    print("Wi-Fi enabled")
+    if not is_wifi_enabled():
+        subprocess.run(['sudo', 'ifconfig', 'wlan0', 'up'])
+        print("Wi-Fi enabled")
+    else:
+        print("Wi-Fi already enabled")
 
 def disable_wifi():
-    subprocess.run(['sudo', 'ifconfig', 'wlan0', 'down'])
-    print("Wi-Fi disabled")
+    if is_wifi_enabled():
+        subprocess.run(['sudo', 'ifconfig', 'wlan0', 'down'])
+        print("Wi-Fi disabled")
+    else:
+        print("Wi-Fi already disabled.")
