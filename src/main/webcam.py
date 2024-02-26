4775483df69492263370f7f867ce7c796dd61509
nhensley <noahehensley@gmail.com>
2024-02-26 11:11:19 -0500

    Added a script that captures images using command line.

--- src/main/webcam.py
+++ src/main/webcam.py
@@ -1,26 +1,25 @@
 import os
-import uuid
-import cv2
+import subprocess
+from datetime import datetime
 
-def capture_image(file_path):
-    # Open a connection to the webcam
-    cap = cv2.VideoCapture(0)  # 0 indicates the first connected webcam
+SAVE_DIRECTORY = "../../images"
+WEBCAM_DEVICE_PATH = "/dev/video0"
+
+def capture_image(file_path,device_path):
+    # Formats the current date/time to a string
+    formatted_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     
-    # Capture a frame
-    ret, frame = cap.read()
+    # Creates a custome image filepath using the current date/time
+    unique_image_file_path = os.path.join(file_path, f"{formatted_datetime}.jpg")
     
-    # Save the captured frame as an image
-    cv2.imwrite(file_path, frame)
+    # Constructs an fswebcam command using the unique filepath
+    cmd = f"fswebcam -r 1920x1080 -p YUYV -S 10 -D 2 -F 2 -d {device_path} {unique_image_file_path}"
     
-    # Release the webcam
-    cap.release()
+    subprocess.run(cmd, shell=True)
+    
+    return unique_image_file_path
 
 if __name__ == "__main__":
-    relative_path = "../images"
-    unique_filename = str(uuid.uuid4()) + ".jpg"
-    image_file_path = os.path.join(relative_path, unique_filename)  
+    captured_image_path = capture_image(SAVE_DIRECTORY,WEBCAM_DEVICE_PATH)
     
-    # Call the function to capture an image
-    capture_image(image_file_path)
-    
-    print("Image captured and saved at:", image_file_path)
+    # print("Image captured and saved at:", captured_image_path)
