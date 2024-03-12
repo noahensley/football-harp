842463d0df8709cea146860c93fa2f684365af49
nhensley <noahehensley@gmail.com>
2024-03-12 11:19:52 -0400

    Added looping for multiple webcam devices.

--- src/main/main.py
+++ src/main/main.py
@@ -12,33 +12,37 @@ CUTOFF_ALTITUDE = 1524  # meters
 
 # webcam.py
 SAVE_DIRECTORY = "../../images"
-WEBCAM_DEVICE_PATH = "video0"
+WEBCAM_DEVICES = ["video0"] # Add more from USB hub
 
 LOOP_NUM = 0
 
 if __name__ == "__main__":
     while True:
         telemetry = ""
-        print("Entering bmp280.py...") if DIREWOLF_MODE is False else None
+        print("Entering bmp280.py...") if not DIREWOLF_MODE else None
         try:
             telemetry += bmp280.read_sensor()
         except Exception as e:
-            print(f"Unable to read from bmp280: {e}") if DIREWOLF_MODE is False else None
+            print(f"Unable to read from bmp280: {e}") if not DIREWOLF_MODE else None
 
-        print("Entering vfan.py...") if DIREWOLF_MODE is False else None
+        print("Entering vfan.py...") if not DIREWOLF_MODE else None
         telemetry += vfan.read_gps()
 
         if len(telemetry) != 0:
-            print(f"Data: {telemetry}") if DIREWOLF_MODE is False else None # Send this string to Direwolf
+            print(f"Data: {telemetry}") if not DIREWOLF_MODE else None # Send this string to Direwolf
         else:
-            print("Unable to collect data.\nRetrying...") if DIREWOLF_MODE is False else None
+            print("Unable to collect data.\nRetrying...") if not DIREWOLF_MODE else None
 
-        print("Entering webcam.py...") if DIREWOLF_MODE is False else None
-        try:           
-            image_filepath = webcam.capture_image(SAVE_DIRECTORY, WEBCAM_DEVICE_PATH)
-            print(f"Image captured and saved at {image_filepath}") if DIREWOLF_MODE is False else None
+        print("Entering webcam.py...") if not DIREWOLF_MODE else None
+        try:
+            images_saved = webcam.capture_images(SAVE_DIRECTORY, WEBCAM_DEVICES)
+            for i in range(len(images_saved)):
+                if i < len(images_saved) - 1:
+                    print(f"Image saved at {images_saved[i]}")
+                else:
+                    print(f"{images_saved[i]} image(s) saved.")
         except Exception as e:
-            print(f"Unable to capture image: {e}") if DIREWOLF_MODE is False else None
+            print(f"Unable to capture image: {e}") if not DIREWOLF_MODE else None
 
         if DIREWOLF_MODE:
             break
--- src/main/webcam.py
+++ src/main/webcam.py
@@ -2,30 +2,38 @@ import os
 import subprocess
 from datetime import datetime
 
-def is_webcam_connected(device_path):
+def is_webcam_connected(webcam):
     # Get the list of devices in /dev directory
     device_list = subprocess.check_output(["ls", "/dev"]).decode("utf-8")
     
     # Check if the device path is present in the list of devices
-    if device_path in device_list:
+    if webcam in device_list:
         return True
     else:
         return False
 
-def capture_image(file_path, device_path):
-    # Ensures webcam is connected
-    if not is_webcam_connected(device_path):
-        raise RuntimeError("Webcam disconnected or invalid device path.")
+def capture_images(file_path, webcam_devices):
+    success_count = 0
+    unique_paths = []
+    for webcam in webcam_devices:
+        # Ensures webcam is connected
+        if not is_webcam_connected(webcam):
+            raise RuntimeError("Webcam disconnected or invalid device path.")
         
-    # Formats the current date/time to a string
-    formatted_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        success_count += 1
+        # Formats the current date/time to a string
+        formatted_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    # Creates a custom image filepath using the current date/time
-    unique_image_file_path = os.path.join(file_path, f"{formatted_datetime}.jpg")
+        # Creates a custom image filepath using the current date/time
+        unique_image_file_path = os.path.join(file_path, f"{formatted_datetime}.jpg")
 
-    # Constructs an fswebcam command using the unique filepath
-    cmd = f"fswebcam -r 1920x1080 -p YUYV -S 10 -D 2 -F 2 -d /dev/{device_path} {unique_image_file_path} > /dev/null 2>&1"
+        # Constructs an fswebcam command using the unique filepath
+        cmd = f"fswebcam -r 1920x1080 -p YUYV -S 10 -D 2 -F 2 -d /dev/{webcam} {unique_image_file_path} > /dev/null 2>&1"
+        
+        unique_paths.append(unique_image_file_path)
 
-    subprocess.run(cmd, shell=True)
-
-    return unique_image_file_path
+        subprocess.run(cmd, shell=True)
+    
+    unique_paths.append(str(success_count))
+    
+    return unique_paths
