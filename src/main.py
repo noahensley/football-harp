f635d4ffd21e03d37d1a2445f5e23b12b97eae87
Noah Hensley <noahehensley@gmail.com>
2025-11-09 12:48:34 -0500

    Main is for Tx, Rx is handled elsewhere.

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ import vfan
 import time
 import webcam
 import aprs_tx
-import aprs_rx
-import threading
 
 # main.py
 LOOP_TIME_DELAY = 10  # Delay in seconds of main loop
@@ -31,22 +29,7 @@ CALLSIGN = "KE8ZXE"
 SSID = "11"
 BAUD_RATE = 1200
 SAMPLE_RATE = 48000
-# rx
-"""
-DIREWOLF_HOST = 'localhost'
-DIREWOLF_PORT = 8001 # KISS TCP port
-received_cutdown_cmd = False
-cutdown_lock = threading.Lock(False)
-th_recieve_cmd = threading.Thread(
-    target=aprs_rx.receive_direwolf_packets, 
-    args=(cutdown_lock, 
-        received_cutdown_cmd,
-        DIREWOLF_HOST,
-        DIREWOLF_PORT,
-        CALLSIGN+"-"+SSID,
-        True)
-)
-"""
+
 
 if __name__ == "__main__":
 
