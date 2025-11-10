4e0e727b1b9afd896881276aea56da13defc4f4e
Noah Hensley <raspberrypi0w@ssu>
2025-11-10 16:37:21 +0000

    Added body of cutdown script. Works with LED.

--- utils/cutdown.py
+++ utils/cutdown.py
@@ -1,6 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+OUTPUT_PIN = 40
+TIME_HIGH = 1
+
 def main():
-    print("Process cutdown")
-    pass
+    try:
+	#print("Setting GPIO mode...")
+    	GPIO.setmode(GPIO.BOARD)
+    	#print(f"Setting pin {OUTPUT_PIN} as GPIO.OUT (initial == LOW)...")
+    	GPIO.setup(OUTPUT_PIN, GPIO.OUT, initial=GPIO.LOW)
+    	print(f"Setting pin {OUTPUT_PIN} HIGH...", end="")
+    	GPIO.output(OUTPUT_PIN, GPIO.HIGH)
+    	print("DONE")
+    	print(f"SIGNAL IS ACTIVE (pin == {OUTPUT_PIN})")
+    	time.sleep(TIME_HIGH)
+    	print(f"Setting pin {OUTPUT_PIN} LOW...", end="")
+    	print("DONE")
+    	GPIO.output(OUTPUT_PIN, GPIO.LOW)
+    	GPIO.cleanup(OUTPUT_PIN)
+    except Exception as e:
+    	print("Error manipulating GPIO:", e)
+
 
 if __name__ == "__main__":
     main()
