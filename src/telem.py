368418b46bb8932dc983aa50e2baef19df72c418
Noah Hensley <raspberrypi0w@ssu>
2025-11-20 16:16:32 -0500

    Added timestamp to telemetry logging.

--- src/telem.py
+++ src/telem.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import time
+from datetime import datetime
 
 ROOT_DIR = Path(__file__).parent.parent
 TELEM_DIR = ROOT_DIR / "./data"
@@ -12,7 +14,8 @@ def log_data(data, file=LOG_FNAME):
         with open(file, "a+", encoding="utf-8") as fp:
             if os.path.getsize(file) > 0:
                 fp.write("\n")
-            bytes = fp.write(data)
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            bytes = fp.write(f"[{timestamp}] {data}")
     except Exception as e:
     	raise
     print(f"DONE ({bytes} bytes logged)") 
